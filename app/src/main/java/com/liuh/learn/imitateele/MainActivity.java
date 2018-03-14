b0cbb2278c175fb2f2da65054849d279052af041
liuhuan2015 <2210712252@qq.com>
2018-03-14 21:36:22 +0800

    工程初始化。

---
+++ app/src/main/java/com/liuh/learn/imitateele/MainActivity.java
@@ -0,0 +1,16 @@
+package com.liuh.learn.imitateele;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 仿饿了么收货地址
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
